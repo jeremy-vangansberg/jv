6a3be03bd9d240a56762308b82ef1e9f86fc6d1c
jeremy Vangansberg <jeremy.vangansberg@gmail.com>
2022-11-26 22:03:27 +0100

    initializing project

---
+++ jeremy_vangansberg/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for jeremy_vangansberg project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jeremy_vangansberg.settings')
+
+application = get_asgi_application()
