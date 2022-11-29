e8012e10c5e2f5900c8eccc844d2ee7875d33387
jeremy Vangansberg <jeremy.vangansberg@gmail.com>
2022-11-29 10:52:18 +0100

    article prepa

--- jeremy_vangansberg/settings.py
+++ jeremy_vangansberg/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'divers'
+    'divers',
+    'tuto',
 ]
 
 MIDDLEWARE = [
@@ -118,9 +119,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [ 
+STATICFILES_DIRS = [
     BASE_DIR / 'static'
-    ]
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- jeremy_vangansberg/urls.py
+++ jeremy_vangansberg/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('divers.urls')),
+    path('tutos/', include('tuto.urls')),
 ]
