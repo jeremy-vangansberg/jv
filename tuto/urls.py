9192a662e569666e8de10f69d8470f7031aa136a
jeremy Vangansberg <jeremy.vangansberg@gmail.com>
2022-12-02 00:00:56 +0100

    fixing ovh issues

--- tuto/urls.py
+++ tuto/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('heroku_django/', views.django_heroku, name='django_heroku')
+    path('heroku_django/', views.django_heroku, name='django_heroku'),
+    path('django_project/', views.django_project, name='django_project'),
+    path('git_practical/', views.django_project, name='git_practical'),
 ]
--- tuto/views.py
+++ tuto/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
-
 
 def django_heroku(request):
     return render(request, 'tuto/django_heroku.html')
+
+
+def django_project(request):
+    return render(request, 'tuto/django_project.html')
+
+
+def git_practical(request):
+    return render(request, 'tuto/git_practical.html')
