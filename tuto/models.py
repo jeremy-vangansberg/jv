9cfd71af39cd9e520789e3c7149711c2c5be74f7
Jeremy <jeremy.vangansberg@gmail.com>
2022-12-08 19:56:10 +0100

    adding api request

--- tuto/models.py
+++ tuto/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class ApiModel(models.Model):
+    description = models.TextField(max_length=1000, blank=False, null=False)
+    url = models.CharField(max_length=100, blank=False, null=False)
--- tuto/views.py
+++ tuto/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-
+from .forms import ApiForm
+import requests
+import json
 
 def django_heroku(request):
     return render(request, 'tuto/django_heroku.html')
@@ -11,3 +13,17 @@ def django_project(request):
 
 def git_practical(request):
     return render(request, 'tuto/git_practical.html')
+
+
+def cons_api(request):
+    if request.method == 'POST':
+        form = ApiForm(request.POST)
+        if form.is_valid():
+            r = requests.post('https://muti-inputs-api-multi-input.cpyusm.easypanel.host/predict', json=form.cleaned_data)
+            print(form.cleaned_data)
+            form.save()
+            r = json.loads(r.text)
+            return render(request, 'tuto/cons_api.html', context = {'form':form, 'response' : r})
+    else :
+        form = ApiForm()
+    return render(request, 'tuto/cons_api.html', context = {'form':form})
