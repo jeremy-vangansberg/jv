07907ef07f751d556bedb8313c66b60019b0b225
jeremy Vangansberg <jeremy.vangansberg@gmail.com>
2024-02-09 11:38:57 +0100

    starting cv redaction

--- web/divers/urls.py
+++ web/divers/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home_page, name='home')
+    path('', views.home_page, name='home'),
+    path('cv/', views.cv, name='cv'),
+    path('projets/', views.projets, name='projets'),
+    path('ressources/', views.ressources, name='ressources'),
 ]
--- web/divers/views.py
+++ web/divers/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 
 def home_page(request):
-
     return render(request, 'divers/home_page.html')
+
+def cv(request):
+    return render(request, 'divers/cv.html')
+
+def projets(request):
+    return render(request, 'divers/projets.html')
+
+def ressources(request):
+    return render(request, 'divers/ressources.html')
